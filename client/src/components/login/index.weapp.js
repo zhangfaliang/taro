c1b844f05c0b0d188ca3888fd71c1972fd802a8f
张发亮 <fl.zhang@8win.com>
2019-06-03 19:03:31 +0800

    初始化

--- client/src/components/login/index.weapp.js
+++ client/src/components/login/index.weapp.js
@@ -1,10 +1,10 @@
-import Taro, { Component } from "@tarojs/taro"
-import { View, Text, Button } from "@tarojs/components"
+import Taro, { Component } from "@tarojs/taro";
+import { View, Text, Button } from "@tarojs/components";
 
 export default class Login extends Component {
   state = {
     context: {}
-  }
+  };
 
   componentWillMount() {}
 
@@ -20,21 +20,46 @@ export default class Login extends Component {
     Taro.cloud
       .callFunction({
         name: "login",
-        data: {}
+        data: { shiny: "shiny" }
       })
       .then(res => {
         this.setState({
           context: res.result
-        })
-      })
-  }
+        });
+      });
+    // 让用户选择一张图片
+    // Taro.chooseImage({
+    //   success: chooseResult => {
+    //     // 将图片上传至云存储空间
+    //     Taro.cloud.uploadFile({
+    //       // 指定上传到的云路径
+    //       cloudPath: "my-photo.png",
+    //       // 指定要上传的文件的小程序临时文件路径
+    //       filePath: chooseResult.tempFilePaths[0],
+    //       // 成功回调
+    //       success: res => {
+    //         console.log("上传成功", res);
+    //       }
+    //     });
+    //   }
+    // });
+
+    // 当然 promise 方式也是支持的
+    Taro.cloud.callFunction({
+      name: 'add',
+      data: {
+        a: 12,
+        b: 19
+      }
+    }).then(console.log)
+  };
 
   render() {
     return (
-      <View className='index'>
+      <View className="index">
         <Button onClick={this.getLogin}>获取登录云函数</Button>
         <Text>context：{JSON.stringify(this.state.context)}</Text>
       </View>
-    )
+    );
   }
 }
--- cloud/functions/login/index.js
+++ cloud/functions/login/index.js
@@ -1,14 +1,17 @@
 const cloud = require('wx-server-sdk')
 
-cloud.init({env:'chiji-test'})
+cloud.init({env:'chiji-test',traceUser: true})
 
 
-exports.main = async () => {
+exports.main = async (event, context) => {
+  let { userInfo, a, b} = event
   const wxContext = cloud.getWXContext()
   console.log(wxContext,'wxContext')
   return {
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+    // openid: wxContext.OPENID,
+    // appid: wxContext.APPID,
+    // unionid: wxContext.UNIONID,
+    // userInfo,userInfo,
+    message:'wernjwekrhiwjekrhjwekrhjwekrhjwe'
   }
 }
