7821c6e65d96dfb5d4e80082029a3b04e30795e9
张发亮 <fl.zhang@8win.com>
2019-06-03 16:42:09 +0800

    conf

--- cloud/functions/login/index.js
+++ cloud/functions/login/index.js
@@ -1,11 +1,11 @@
 const cloud = require('wx-server-sdk')
 
-cloud.init()
+cloud.init({env:'chiji-test'})
 
 
 exports.main = async () => {
   const wxContext = cloud.getWXContext()
-
+  console.log(wxContext,'wxContext')
   return {
     openid: wxContext.OPENID,
     appid: wxContext.APPID,
