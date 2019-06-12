79bde43aae974ad5f753921d5682249e64ed488e
张发亮 <fl.zhang@8win.com>
2019-06-12 16:28:47 +0800

    add  搜索

---
+++ client/src/components/Search/index.js
@@ -0,0 +1,43 @@
+import Taro, { Component } from "@tarojs/taro";
+import withWeapp from "@tarojs/with-weapp";
+import {
+    View,
+    Image,
+    Input,
+    Text
+  } from "@tarojs/components";
+import   './index.scss';
+
+class Search extends Component{
+    onSearch =(e)=>{
+        e.stopPropagation();
+      console.log(e)
+      //../../images/search.png
+    }
+    onInput =(e)=>{
+        // e.stopPropagation();
+      console.log(e)
+      //../../images/search.png
+    }
+    render(){
+        return(
+            <View className="search flex-wrp">
+            <View className="search-left flex-item">
+              <Image src={require("../../images/search.png")} />
+              <Input
+                placeholder="搜索话题, 问题或人"
+                placeholderClass="search-placeholder"
+                value={this.state.value}
+                onInput={this.onInput}
+
+              />
+            </View>
+            <View className="search-right flex-item" onClick={this.onSearch}>
+              <Image src={require("../../images/lighting.png")} />
+            </View>
+          </View>
+        )
+    }
+}
+
+export default Search;
